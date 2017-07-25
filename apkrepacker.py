5e4be185acd6786058f3aa4d581e538fd63505da
Aleksandr PILGUN <aleksandr.pilgun@uni.lux>
2017-07-26 00:30:27 +0200

    logging&exceptions fixes

--- apkrepacker.py
+++ apkrepacker.py
@@ -28,11 +28,6 @@ temp_dir = r'C:\projects\comma\output\temp'
 def request_pipe(cmd):
     pipe = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     out, err = pipe.communicate()
-   
-    if logger.enabled:
-        logger.log(out)
-        logger.log(err)
-
     return out, err, pipe.returncode
 
 def read_csv(path):
@@ -48,23 +43,26 @@ def repackage(output_apk, input_apk, name):
     clean_temp(temp_dir)
     cmd_decode = "java -jar {0} d --force --no-debug-info -o {1} {2}".format(apktool_path, temp_dir, input_apk)
     out_d, err_d, code_d = request_pipe(cmd_decode)
-    logger.log("PATH: {0}BUILD:\nOUT: {1}\nERR: {2}\nCODE: {3}".format(name, out_d, err_d, code_d))
+    #logger.log("PATH: {0}BUILD:\nOUT: {1}\nERR: {2}\nCODE: {3}".format(name, out_d, err_d, code_d))
     
     cmd_build = "java -jar {0} b --force-all -o {1} {2}".format(apktool_path, output_apk, temp_dir)
     out_b, err_b, code_b = request_pipe(cmd_build)
-    logger.log("PATH: {0}BUILD:\nOUT: {1}\nERR: {2}\nCODE: {3}".format(name, out_b, err_b, code_b))
+    #logger.log("PATH: {0}BUILD:\nOUT: {1}\nERR: {2}\nCODE: {3}".format(name, out_b, err_b, code_b))
 
-    success = os.path.exists(output_apk)
+    success = str(os.path.exists(output_apk))
     return apktool_rslt(success, out_d, out_b, err_d, err_b, code_d, code_b)
 
 def clean_temp(dir):
     if os.path.exists(dir):
-        shutil.rmtree(dir)
+        #Hack for windows. Shutil can't remove files with paths longer 260.
+        cmd = "rd {0} /s /q".format(dir)
+        request_pipe(cmd)
+        #shutil.rmtree(dir)
     os.makedirs(dir)
 
 def save_csv(path, list_of_apks):
     with open(path, 'wb') as f:
-        writer = csv.writer(f, delimiter=',')
+        writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(apk_info.get_title())
         for app in list_of_apks:
             writer.writerow(app.get_row())
@@ -76,24 +74,30 @@ def main():
     i = 0
     print("Repackaging of {0} apks.".format(len(list_of_apks)))
     for app in list_of_apks:
-        print("{0} - original - {1}".format(i, app.original))
-        original_in_apk = os.path.join(input_apk_dir, app.original)
-        original_out_apk = os.path.join(output_apk_dir, app.original)
-        result = repackage(original_out_apk, original_in_apk, name=app.original)
-        app.set_original_apktool_result(result)
+        try:
+            original_in_apk = os.path.join(input_apk_dir, app.original)
+            original_out_apk = os.path.join(output_apk_dir, app.original)
+            result = repackage(original_out_apk, original_in_apk, name=app.original)
+            app.set_original_apktool_result(result)
+            logger.log("-".join(app.get_row()))
+            print("{0} - original - {1} - success: {2}".format(i, app.original, app.success1))
 
-        print("{0} - piggy - {1}".format(i, app.piggy))
-        piggy_in_apk = os.path.join(input_apk_dir, app.piggy)
-        piggy_out_apk = os.path.join(output_apk_dir, app.piggy)
-        result = repackage(piggy_out_apk, piggy_in_apk, app.piggy)
-        app.set_piggy_apktool_result(result)
+        except Exception as e:
+            logger.log("FAILED-{0}: {1}".format(app.original, e))
+        
+        
+        # print("{0} - piggy - {1}".format(i, app.piggy))
+        # piggy_in_apk = os.path.join(input_apk_dir, app.piggy)
+        # piggy_out_apk = os.path.join(output_apk_dir, app.piggy)
+        # result = repackage(piggy_out_apk, piggy_in_apk, app.piggy)
+        # app.set_piggy_apktool_result(result)
         i += 1
         
     save_csv(os.path.join(base_dir,csv_output_name), list_of_apks)
 
 class apk_info(object):
-    def __init__(self, original='', piggy='', res_num1=0, res_num2=0, 
-        f_score=0.0, certificates=''):
+    def __init__(self, original='', piggy='', res_num1='', res_num2='', 
+        f_score='', certificates=''):
         self.original = original
         self.piggy = piggy
         self.res_num1 = res_num1
@@ -176,18 +180,18 @@ class apk_info(object):
                 "code_dec1",
                 "code_dec2",
                 "code_build1",
-                "code_build"]
+                "code_build2"]
 
 class apktool_rslt(object):
     def __init__(self, success, out_decode, out_build, err_decode, err_build,
         code_decode, code_build):
         self.success = success
-        self.out_decode=out_decode.replace('\n', ' -> ')
-        self.out_build = out_build.replace('\n', ' -> ')
-        self.err_decode = err_decode.replace('\n', ' -> ')
-        self.err_build = err_build.replace('\n', ' -> ')
-        self.code_decode = code_decode
-        self.code_build = code_build
+        self.out_decode=''#out_decode.replace('\n', ' -> ')
+        self.out_build = ''#out_build.replace('\n', ' -> ')
+        self.err_decode = err_decode.replace('\r\n', ' -> ').replace('\n',' ')
+        self.err_build = err_build.replace('\r\n', ' -> ').replace('\n',' ')
+        self.code_decode = str(code_decode)
+        self.code_build = str(code_build)
 
 if __name__ == '__main__':
     main()
